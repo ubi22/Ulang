1b321bbac022f459dd365405b272d188ccc2c06f
ubi22 <zoom.main21@gmail.com>
2024-12-13 00:11:55 +0300

    correction commit

--- deletbase.py
+++
@@ -1,9 +0,0 @@
-from authorization import authorization_admin
-
-pb = authorization_admin()
-def delete_base(name_base):
-    for i in pb.collection(name_base).get_full_list():
-        pb.collection(name_base).delete(i.id)
-    print("Succses")
-
-delete_base("baseword")
